4ca283bb138e53d6530a297de5d4006d0f6e3f12
jkenjayev <javohirwebdev@gmail.com>
2021-01-09 01:35:50 +0500

    Added post and put routes with buges ;)

--- index.js
+++ index.js
@@ -21,20 +21,37 @@ app.get("/api/categories/:id", (req, res) => {
   res.send(category);
 });
 
-app.post("/api/categories", (req, res) => {
+app.post("api/categories", (req, res) => {
     const { error } = validateCategory(req.body);
     if(error) return res.status(400).send(error.details[0].message);
-    
+
     const category = {id: categories.length + 1, title: req.body.title};
     categories.push(category);
 
     res.status(201).send(category);
 });
 
-function validateCategory(category) {
-    const schema = {
-        title: Joi.string().required().min(4)
-    }
+app.put("/api/categories/:id", (req, res) => {
+    const { error } = validateCategory(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
 
-    return Joi.validate(category, schema);
+    const category = categories.find(ctg => ctg.id === parseInt(req.params.id));
+    if(!category) return res.status(404).send('Not found such category');
+
+    category.title = req.body.title
+
+    res.send(category);
+})
+
+function validateCategory(category) {
+  const schema = {
+    title: Joi.string().required().min(4),
+  };
+
+  return Joi.validate(category, schema);
 }
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Hey! I am listening the ${port}th port`);
+});
